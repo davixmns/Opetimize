92c31dcd4add87139f311787e627a434e878f65a
daviximenes <daviximenes@unifor.br>
2023-04-14 14:34:33 -0300

    save, findById, findAll

--- index.js
+++ index.js
@@ -1,54 +1,76 @@
-///config inicial
+///CONFIGURAÇÃO INICIAL
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-
 const Purchase = require('./models/Purchase')
 
-//forma de ler json / middlewares
+//MIDDLEWARES
 app.use(express.urlencoded({
     extended: true,
 }),)
 app.use(express.json())
 
-//rotas da api
+//CONEXÃO COM O MONGO
+function startConnection() {
+    require('dotenv').config();
+    const DB_USER = process.env.DB_USER;
+    const DB_PASSWORD = process.env.DB_PASSWORD;
+    const PORT = 3000
+    mongoose
+        .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@zeuscluster.ibjagkj.mongodb.net/Racoes?retryWrites=true&w=majority`)
+        .then(() => {
+            console.log("conectamos ao mongodb")
+            app.listen(PORT, () => {
+                console.log(`Servidor iniciado na porta ${PORT}`)
+            })
+        })
+        .catch((err) => console.log(err))
+}
+startConnection()
+
+//POST - CADASTRA UMA COMPRA DE RAÇÃO
 app.post('/purchase', async (req, res) => {
     const {name, price, weight, date} = req.body
+    if (!name) {
+        res.status(422).json({error: "Compra não cadastrada, o nome é obrigatório"})
+    } else if (!price) {
+        res.status(422).json({error: "Compra não cadastrada, o preço é obrigatório"})
+    } else if (!weight) {
+        res.status(422).json({error: "Compra não cadastrada, o peso é obrigatório"})
+    }
     const purchase = {
         name,
         price,
         weight,
         date
     }
-    try{
-        await Purchase.create(purchase) //criando dados
+    try {
+        await Purchase.create(purchase)
         res.status(201).json({message: "Compra de ração foi cadastrada"})
-    }catch (error){
+    } catch (error) {
         res.status(500).json({error: error})
     }
 })
 
-
-//rota inicial / endpoint
-app.get('/', (req, res) => {
-    //mostrar req
-    //teste
-    res.json({message: "oi express!"})
+//GET - CAPTURA TODAS AS COMPRAS DE RAÇÕES
+app.get("/purchases", async (req, res) => {
+    const allPurchases = await Purchase.find()
+    res.status(200).json({ resposta: allPurchases })
 })
 
-//entregar uma porta
-require('dotenv').config();
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+//GET - CAPTURA UMA COMPRA DE RAÇÃO POR ID
+app.get("/purchases/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const purchase = await Purchase.findById(id);
+        if (!purchase) {
+            res.status(404).json({ message: "Compra não encontrada." });
+        }
+        res.status(200).json({ resposta: purchase });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
 
-const PORT = 3000
+//PUT - EDITAR COMPRA DE RAÇÃO POR ID
 
-mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@zeuscluster.ibjagkj.mongodb.net/test?retryWrites=true&w=majority`)
-    .then(() => {
-        console.log("conectamos ao mongodb")
-        app.listen(PORT, () => {
-            console.log(`Servidor iniciado na porta ${PORT}`)
-        })
-    })
-    .catch((err) => console.log(err))
