e4f3b31c568e53729e06510703ec4bfb36e59102
daviximenes <daviximenes@unifor.br>
2023-04-14 16:54:45 -0300

    react

---
+++ mobile/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.input}>O Zeus é braboooooooooooo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: 'blue'
+  }
+});
