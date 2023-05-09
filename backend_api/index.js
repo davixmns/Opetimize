09c3b9dcce5035f5d2ce5e431735f3e149679aaf
daviximenes <daviximenes@unifor.br>
2023-05-09 11:57:49 -0300

    arquitetura mvc

--- backend_api/index.js
+++ backend_api/index.js
@@ -1,24 +1,14 @@
-///CONFIGURAÇÃO INICIAL
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
-const Purchase = require('./models/Purchase')
+const purchaseController = require("controllers/purchaseController.js")
 const cors = require('cors');
-const {all} = require("express/lib/application");
 
-
-app.use(cors());
-
-// Restante do código da sua API em Node
-
-
-//MIDDLEWARES
-app.use(express.urlencoded({
-    extended: true,
-}),)
+const app = express()
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-//CONEXÃO COM O MONGO
+//conexao
 function startConnection() {
     require('dotenv').config();
     const DB_USER = process.env.DB_USER;
@@ -36,86 +26,18 @@ function startConnection() {
 }
 startConnection()
 
-//POST - CADASTRA UMA COMPRA DE RAÇÃO
-app.post('/purchase', async (req, res) => {
-    const {name, price, weight, date} = req.body
+//rotas
+const router = express.Router()
+router.get("/purchases", purchaseController.getAllPurchases)
+router.get("/purchases/:id", purchaseController.getPurchaseById)
+router.post("/purchases", purchaseController.createPurchase)
+router.put("/purchases/:id", purchaseController.updatePurchaseById)
+router.delete("/purchases/:id", purchaseController.deletePurchaseById)
 
-    const purchase = {
-        name,
-        price,
-        weight,
-        date
-    }
-    try {
-        await Purchase.create(purchase)
-        res.status(201).json({message: "Compra de ração foi cadastrada"})
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
-})
+app.use('/', router);
 
-//GET - CAPTURA TODAS AS COMPRAS DE RAÇÕES
-app.get("/purchases", async (req, res) => {
-    const allPurchases = await Purchase.find()
-    res.status(200).json(allPurchases)
-})
-
-//GET - CAPTURA UMA COMPRA DE RAÇÃO POR ID
-app.get("/purchases/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const purchase = await Purchase.findById(id);
-        if (!purchase) {
-            res.status(404).json({message: "Compra de ração não encontrada."});
-        }
-        res.status(200).json({resposta: purchase});
-    } catch (error) {
-        res.status(500).json({error: error});
-    }
+// Inicia o servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado na porta ${PORT}`);
 });
-
-//PUT - EDITAR COMPRA DE RAÇÃO POR ID
-app.put("/purchases/:id", async (req, res) => {
-    try {
-        const id = req.params.id
-        const oldPurchase = await Purchase.findById(id)
-        if (!oldPurchase) {
-            res.status(404).json({message: "Compra de ração não encontrada"})
-        }
-        const {name, price, weight, date} = req.body
-        const newPurchase = {name, price, weight, date}
-
-        await Purchase.findByIdAndUpdate(id, newPurchase)
-        res.status(200).json({message: "Compra de ração atualizada com sucesso"})
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
-})
-
-//DELETE - DELETAR COMPRA DE RAÇÃO POR ID
-app.delete("/purchases/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const purchase = await Purchase.findById(id);
-        if (!purchase) {
-            res.status(404).json({message: "Compra de ração não encontrada"});
-            return;
-        }
-        await Purchase.deleteOne({_id: id});
-        res.status(200).json({message: "Compra de ração deletada com sucesso!"});
-    } catch (error) {
-        res.status(500).json({error: error});
-    }
-});
-
-app.delete("/purchases", async (req, res) => {
-    try {
-        await Purchase.deleteMany();
-        res.status(200).json({ message: "Todas as compras foram deletadas com sucesso." });
-    } catch (error) {
-        res.status(500).json({ error: error });
-    }
-});
-
-
-
--- backend_api/models/purchaseModel.js
+++ backend_api/models/purchaseModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const Purchase = mongoose.model('Purchase', {
+const PurchaseModel = mongoose.model('Purchase', {
     name: String,
     price: Number,
     weight: Number,
     date: Date
 })
 
-module.exports = Purchase
+module.exports = PurchaseModel
