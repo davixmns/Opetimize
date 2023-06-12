7aeb90fed377b0ef40024360f8d7201548e0065b
daviximenes <daviximenes@unifor.br>
2023-06-12 17:23:41 -0300

    DOCKERIZANDO

---
+++ docker/api/db/script.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS opetimize;
+USE opetimize;
+
+CREATE TABLE users
+(
+    user_id       INT PRIMARY KEY AUTO_INCREMENT,
+    name          VARCHAR(50)  NOT NULL,
+    email         VARCHAR(100) NOT NULL UNIQUE,
+    password      VARCHAR(100) NOT NULL,
+    profile_image VARCHAR(100)
+);
+
+CREATE TABLE purchases
+(
+    purchase_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id     INT         NOT NULL,
+    name        VARCHAR(50) NOT NULL,
+    price       FLOAT       NOT NULL,
+    weight      FLOAT       NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE pets
+(
+    pet_id  INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT         NOT NULL,
+    breed   VARCHAR(50) NOT NULL,
+    name    VARCHAR(50) NOT NULL,
+    age     INT         NOT NULL,
+    weight  FLOAT       NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
--- mongo_api/models/PurchaseModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const PurchaseModel = mongoose.model('Purchase', {
-    name: String,
-    price: Number,
-    weight: Number,
-    date: Date
-})
-
-module.exports = PurchaseModel
